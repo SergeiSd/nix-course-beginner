0f42866ab8aff0297edef811e9102457756600d6
Slyadnev Sergey Aleksandrovich <42943522+SergeiSd@users.noreply.github.com>
2021-06-16 23:13:39 +0300

    Converts sentences to uppercase.
    The fourth practice assignment of course Python\Beginner

---
+++ strings_upper.py
@@ -0,0 +1,48 @@
+import argparse
+import codecs
+import os
+import sys
+import nltk
+# nltk.download('punkt')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--path_file', default='strings.txt',
+                    help='The location of file.')
+args = parser.parse_args()
+
+
+def get_strings(file: str = args.path_file) -> list:
+    """ Reading data from a file and converting it to a list of sentences.
+
+    Args:
+        file: the path to the data file.
+    Returns:
+        a list of sentences.
+    """
+
+    if not os.path.isfile(file):
+        print("Error: {} does not exist.".format(file))
+        sys.exit(1)
+
+    with codecs.open(file, 'r', encoding='utf8') as reader:
+        content = reader.read()
+
+    sents = nltk.sent_tokenize(content)
+    return sents
+
+
+def upper(sents: list = get_strings()) -> list:
+    """ Converts all sentences to uppercase.
+
+    Args:
+        sents: text sentences.
+    Returns:
+        a list of uppercase sentences.
+    """
+
+    return [x.upper() for x in sents if 'price' in x]
+
+
+if __name__ == '__main__':
+    for index, line in enumerate(upper()):
+        print(f'{index+1} line:', line)
